a714beb92888d80b9c0d6affbd69d4a6c6648c8b
kilgore92 <ishaan.bits92@gmail.com>
2017-02-08 01:09:52 +0100

    basic static crop and padding working

--- img_distortion.py
+++ img_distortion.py
@@ -2,6 +2,8 @@
 import cv2
 import numpy as np
 
+
+
 # Read image
 img = cv2.imread('lena.jpg')
 
@@ -15,8 +17,19 @@ x2 = 300
 
 crop_img = img[y1:y2,x1:x2]
 
+# Add borders
+width,height = img.shape[:2]
+
+BLACK = [0,0,0]
+
+padded_img = cv2.copyMakeBorder(crop_img,y1,height-y2,x1,width-x2,cv2.BORDER_CONSTANT,value=BLACK)
 
 # Display images
 cv2.imshow("Output",img)
 cv2.imshow("Cropped",crop_img)
-cv2.waitKey(0)
+cv2.imshow("Padded",padded_img)
+#cv2.waitKey(0)
+
+# Save the images
+cv2.imwrite("cropped.jpg",crop_img)
+cv2.imwrite("padded.jpg",padded_img)
